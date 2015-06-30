38c8d1bca6569c47560a85b31783b88a99925fea
Marius Ungureanu <therzok@Mariuss-MBP.home>
2015-06-30 12:49:59 +0100

    Initial import

---
+++ DiffCopyPastaFixer/DiffCopyPastaFixer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using MonoDevelop.Components.Commands;
+using MonoDevelop.Ide;
+using Mono.TextEditor;
+
+namespace DiffCopyPastaFixer
+{
+	public enum DiffCopyPastaFixerCommands
+	{
+		RemoveDiffLines,
+	}
+
+	public class RemoveDiffLinesHandler : CommandHandler
+	{
+		static int GetIndexOfDiffSymbol (string text)
+		{
+			for (int i = 0; i < text.Length; ++i) {
+				if (Char.IsWhiteSpace (text [i]))
+					continue;
+
+				if (text [i] == '+' || text [i] == '-')
+					return i;
+
+				break;
+			}
+			return -1;
+		}
+
+		protected override void Run ()
+		{
+			var editor = IdeApp.Workbench.ActiveDocument.GetContent<ITextEditorDataProvider> ().GetTextEditorData ();
+
+			foreach (var line in editor.SelectedLines) {
+				var text = editor.GetLineText (line.LineNumber);
+				var start = GetIndexOfDiffSymbol (text);
+				if (start == -1)
+					continue;
+
+				editor.Remove (line.Offset + start, 1);
+			}
+		}
+
+		protected override void Update (CommandInfo info)
+		{
+			var doc = IdeApp.Workbench.ActiveDocument;
+			info.Enabled = doc != null;
+		}
+	}
+}
---
+++ DiffCopyPastaFixer/Properties/AddinInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly:Addin (
+	"DiffCopyPastaFixer", 
+	Namespace = "DiffCopyPastaFixer",
+	Version = "1.0"
+)]
+
+[assembly:AddinName ("DiffCopyPastaFixer")]
+[assembly:AddinAuthor ("Marius Ungureanu")]
+[assembly:AddinCategory ("Source Editor Extensions")]
+[assembly:AddinUrl ("http://monodevelop.com")]
+[assembly:AddinDescription ("Removes leading diff lines from text. Useful when manually applying patches.")]
+[assembly:AddinAuthor ("Marius Ungureanu")]
+
